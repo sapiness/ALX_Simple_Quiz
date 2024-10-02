0a44db63b2c4fcfb8830f3b62fb85ab9a95ab2ff
surprise <sapiness695@gmail.com>
2024-10-02 18:28:41 +0200

    simple quiz updated

--- quiz.js
+++ quiz.js
@@ -1,19 +1,20 @@
 function checkAnswer() {
     // Function body
     let correctAnswer = "4";
-    let userAnswer = document.querySelector("quiz");
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value
     let paragraph = document.getElementById("feedback");
 
     if (userAnswer == correctAnswer){
         paragraph.textContent = "Correct! Well done.";
-      
     }
+   
     else{
-        paragraph.textContent ="That's incorrect. Try again!";
+        paragraph.textContent = "That's incorrect. Try again!";
     }
 
 }
 
 let submit = document.getElementById("submit-answer");
-    submit.addEventListener('click',checkAnswer)
+    submit.addEventListener('click',checkAnswer);
+
 
